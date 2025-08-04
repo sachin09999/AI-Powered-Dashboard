bcdc98ff74b74d821b9a1c5f96b76a499a8d05f7
sachin singh <5516sachinrajput@gmail.com>
2025-08-04 14:46:43 +0000

    when we are on light more glass morphic effect should be the the not whi

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-soft dark:bg-card/40 dark:backdrop-blur-lg",
+      "rounded-lg border bg-card/60 text-card-foreground shadow-soft backdrop-blur-lg dark:bg-card/40",
       className
     )}
     {...props}
